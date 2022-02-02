929a26fa766642b2e2aa046e841d476c667e1a42
jeevan <jkc5186@gmail.com>
2022-02-02 20:49:36 +0545

    upto basic signin signup markup and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Explore from './pages/Explore';
-import Offers from './pages/Offers';
-import Profile from './pages/Profile';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ForgotPassword from './pages/ForgotPassword';
+import Navbar from './components/navbar/Navbar';
+import Explore from './pages/explore/Explore';
+import Offers from './pages/offers/Offers';
+// import Profile from './pages/profile/Profile';
+import SignIn from './pages/signIn/SignIn';
+import SignUp from './pages/signUp/SignUp';
+import ForgotPassword from './pages/forgotPassword/ForgotPassword';
 
 function App() {
   return (
@@ -14,12 +15,12 @@ function App() {
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
-          <Route path='/sign-in' element={<Profile />} />
+          <Route path='/profile' element={<SignIn />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           
         </Routes>
-        {/* Navbar */}
+        <Navbar />
       </Router>
       
     </>
---
+++ src/pages/signUp/SignUp.jsx
@@ -0,0 +1,105 @@
+import './SignUp.scss'
+
+import { useState } from 'react'
+//import { toast } from 'react-toastify'
+//import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+//import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+//import OAuth from '../components/OAuth'
+import { ReactComponent as ArrowRightIcon } from '../../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../../assets/svg/visibilityIcon.svg'
+
+function SignUp() {
+   const [showPassword, setShowPassword] = useState(false)
+   const [formData, setFormData] = useState({
+      name:'',
+      email: '',
+      password: '',
+      
+   })
+   
+   const { name, email, password } = formData
+   
+   //const navigate = useNavigate()
+
+   const onChange = (e) => {
+      setFormData((prevState) => ({
+         ...prevState,
+         [e.target.id]: e.target.value,
+      }))
+   }
+
+   return (
+      <>
+         <div className='pageContainer'>
+            <header>
+               <p className='pageHeader'>Welcome Back!</p>
+            </header>
+
+            <form>
+               <input
+                  type='name'
+                  className='nameInput'
+                  placeholder='Name'
+                  id='name'
+                  value={name}
+                  onChange={onChange}
+               />
+
+               <input
+                  type='email'
+                  className='emailInput'
+                  placeholder='Email'
+                  id='email'
+                  value={email}
+                  onChange={onChange}
+               />
+
+               <div className='passwordInputDiv'>
+                  <input
+                     type={showPassword ? 'text' : 'password'}
+                     className='passwordInput'
+                     placeholder='Password'
+                     id='password'
+                     value={password}
+                     onChange={onChange}
+                  />
+
+                  <img
+                     src={visibilityIcon}
+                     alt='show password'
+                     className='showPassword'
+                     onClick={() => setShowPassword((prevState) => !prevState)}
+                  />
+               </div>
+
+               <Link to='/forgot-password' className='forgotPasswordLink'>
+                  Forgot Password
+               </Link>
+
+               <div className='signInBar'>
+                  <p className='signInText'>Sign In</p>
+                  <button className='signInButton'>
+                  <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+                  </button>
+               </div>
+            </form>
+            {/*Google Oath*/}
+            <Link to='/sign-in' className='registerLink'>
+               Sign In Instead
+            </Link>
+         </div>
+      </>
+   )
+}
+
+export default SignUp
+
+
+
+
+
+
+
+
